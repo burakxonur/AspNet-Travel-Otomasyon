9fa5769533df1d56ac0b5b65914ec60ac0d6e91c
Burak <onurburak149@gmail.com>
2023-09-29 18:02:14 +0300

    context ve model tanımalama
    context ve model tanımalama

---
+++ TravelCoreProje/DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrate
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=BURAK-ONUR\\SQLEXPRESS01; initial catalog =TravelDB;integrated security=true;");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<About2> About2s { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Feature2> Feature2s { get; set; }
+        public DbSet<Guide> Guides { get; set; }
+        public DbSet<Newsletter> Newsletters { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<SubAbout> SubAbouts { get; set; }
+    }
+}
