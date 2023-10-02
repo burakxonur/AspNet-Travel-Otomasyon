0ccba5c190f8702a199f3ab7bdca7664083649be
Burak <onurburak149@gmail.com>
2023-10-02 18:02:55 +0300

    Partial ayarlandı
    Partial ayarlandı

---
+++ TravelCoreProje/BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmı Boş Bırakılamaz");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Bir Fotoğraf Seçiniz.");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen 50 Karakter Açıklama Giriniz.");
+
+        }
+    }
+}
